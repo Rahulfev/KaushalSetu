92cc6ef8a2545f652053fb0322907e5310a2762b
Rahul Singh <134881830+Rahulfev@users.noreply.github.com>
2026-08-18 22:36:13 +0530

    Delete target directory

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
--- Auto-seed roles on startup (safe, skips if already exists)
-INSERT IGNORE INTO roles (role_name) VALUES ('WORKER');
-INSERT IGNORE INTO roles (role_name) VALUES ('CLIENT');
-INSERT IGNORE INTO roles (role_name) VALUES ('ADMIN');
-INSERT IGNORE INTO roles (role_name) VALUES ('ORGANIZATION');
